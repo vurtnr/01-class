6e02a6a5e8a07e0a994db2b1642e59dfafdcb9c3
yumin.jiao <yumin.jiao@trinasolar.com>
2024-04-25 23:08:32 +0800

    update cli first level

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-fn main() {
-    // TODO: Test
-    println!("Hello, world!");
+use clap::Parser;
+use rcli::{process_csv, Opts, SubCommand};
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+    }
+
+    Ok(())
 }
